d6f6d3d0d7223712245611979c06214e3fd85956
Harrison Sayre <hsayre42@gmail.com>
2024-07-07 15:48:40 -0400

    <Finished bonuses>

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -12,6 +12,9 @@ const oldPointStructure = {
   10: ['Q', 'Z']
 };
 
+//Used for isVowel to check for point value in vowelBonusScorer
+const vowels = ["A", "E", "I", "O", "U"];
+
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
@@ -29,30 +32,121 @@ function oldScrabbleScorer(word) {
 	return letterPoints;
  }
 
-// your job is to finish writing these functions and variables that we've named //
-// don't change the names or your program won't work as expected. //
-
+// Your job is to finish writing these functions and variables that we've named //
+// Don't change the names or your program won't work as expected. //
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let word = oldScrabbleScorer(input.question("Let's play some scrabble!\n\nEnter a word to score: "));
 };
 
-let newPointStructure;
+let newPointStructure = transform(oldPointStructure);
+//Adds the blank tile for the bonus. Want transform to be "neutral", so don't hardcode it in the function but do it here. 
+newPointStructure[" "] = 0;
 
-let simpleScorer;
+//let simpleScorer; //do we comment this out? It's declared as a variable, not a function...
 
-let vowelBonusScorer;
+function simpleScorer(word){
+   return(word.length); //If every letter is the same, only thing that matters is how long word is. Does count spaces. Symbols and numbers not allowed as inout, so don't need to check here.
+}
 
-let scrabbleScorer;
+//Used for vowel scoring
+function isVowel(character){
+   return vowels.includes(character);
+}
 
-const scoringAlgorithms = [];
+//let vowelBonusScorer; //do we comment this out? It's declared as a variable, not a function...
+function vowelBonusScorer(word){
+   word = word.toUpperCase();
+   let letterPoints = 0;
+   for (let i = 0; i < word.length; i++){
+      if(isVowel(word[i])){
+         letterPoints += 3
+      }else{
+         letterPoints++;
+      }
+   }
+   return letterPoints;
+}
 
-function scorerPrompt() {}
+//let scrabbleScorer;
+function scrabbleScorer(word){
+   word = word.toLowerCase();
+	let letterPoints = 0;
+	for (let i = 0; i < word.length; i++) {
+      letterPoints += newPointStructure[word[i]]; 
+   }
+   return letterPoints;
+}
 
-function transform() {};
+//Checks to see if a word is legal to play.
+function wordChecker(word){
+   word = word.toLowerCase()
+   //Iterate through input word.
+   for(let i = 0; i < word.length; i++){
+      //If it's not a letter or space it's not legal.
+      if(newPointStructure[word[i]] === undefined){
+         return false;
+      };
+   }
+   //Legal input
+   return true;
+}
+
+let simpleScorerStructure = {
+   name: "Simple Score",
+   description: "Each letter is worth 1 point.",
+   scorerFunction: simpleScorer
+};
+
+let vowelBonusScorerStructure = {
+   name: "Bonus Vowels",
+   description: "Vowels are 3 pts, consonants are 1 pt.",
+   scorerFunction: vowelBonusScorer
+};
+
+let ScrabbleScorerStructure = {
+   name: "Scrabble",
+   description: "The traditional scoring algorithm.",
+   scorerFunction: scrabbleScorer
+};
+
+const scoringAlgorithms = [simpleScorerStructure, vowelBonusScorerStructure, ScrabbleScorerStructure];
+
+function scorerPrompt() {
+   let word = input.question("Let's play some scrabble!\n\nEnter a word to score: ")
+   //Check to see user enters a legal scrabble word.
+   let validWord = wordChecker(word);
+   //Prompts to retype until they do so.
+   while(validWord === false){
+      word = input.question("Please enter a valid word to play in scrabble: ")
+      validWord = wordChecker(word);
+   }
+   let mode = "";
+   //Ensure only a proper mode is picked.
+   while(mode != '0' & mode != '1' & mode != '2'){
+      mode = input.question(`Which scoring algorithm would you like to use?   
+0 - Simple: One point per character
+1 - Vowel Bonus: Vowels are worth 3 points
+2 - Scrabble: Uses scrabble point system
+Enter 0, 1, or 2: `)}
+   score = scoringAlgorithms[mode].scorerFunction(word);
+   console.log(`Score for ${word}: ${score}`);
+}
+
+function transform(structure) {
+   let transformedStructure = {};
+   for(item in structure){
+      for(let i = 0; i < structure[item].length; i++){
+         newKey = structure[item][i];
+         newKey = newKey.toLowerCase(); 
+         transformedStructure[newKey] = Number(item);
+      }
+   }
+   return transformedStructure;
+}
 
 function runProgram() {
-   initialPrompt();
-   
+   //initialPrompt();
+   scorerPrompt();
 }
 
 // Don't write any code below this line //
